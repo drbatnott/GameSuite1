30a9779119866e7b4fbf95dc4a193314b51ec1eb
drbatnott <Zygomy@gmail.com>
2022-03-17 13:27:12 +0000

    Completed The first version up to end of first go
    Still no timer so still no Deployment

--- Game1/Assets/GameControl.cs
+++ Game1/Assets/GameControl.cs
@@ -10,28 +10,55 @@ public class GameControl : MonoBehaviour
     public GameObject cue;
     Transform cueTransform;
     public float rotationSpeed;
+    public GameObject protoTarget;
+    List<GameObject> targets;
+    public int numberOfTargets;
+    bool done;
     // Start is called before the first frame update
     void Start()
     {
         cueBallRigidBody = cueBall.GetComponent<Rigidbody>();
         cueTransform = cue.GetComponent<Transform>();
+        targets = new List<GameObject>();
+        SetUpTargets(numberOfTargets);
+        done = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetUpTargets(int nTargets)
+    {
+        int startNumber = targets.Count;
+        for(int i = startNumber; i< nTargets; i++)
+        {
+            GameObject target = GameObject.Instantiate(protoTarget);
+            SetTargetLocation(target);
+            targets.Add(target);
+        }
+    }
+    void SetTargetLocation(GameObject g)
+    {
+        Transform t = g.transform;
+        Vector3 pos = t.position;
+        float x = Random.Range(-4f, 4f);
+        float z = Random.Range(-4f, 4f);
+        pos.x = x;
+        pos.z = z;
+        t.position = pos;
+    }
+
+    void UpdateCue()
     {
         Vector3 mPosition = Input.mousePosition;
-        float xPos = 20 * ((mPosition.x/1920f) -0.5f);
-        float zPos = 10*((mPosition.y/1080f)- 0.5f);
+        float xPos = 20 * ((mPosition.x / 1920f) - 0.5f);
+        float zPos = 10 * ((mPosition.y / 1080f) - 0.5f);
         //Debug.Log(xPos + " " + zPos);
         Vector3 pos = new Vector3(xPos, 1f, zPos);
         cueTransform.position = pos;
         float h = Input.GetAxis("Horizontal");
-        
-        if (Mathf.Abs(h)>0)
+
+        if (Mathf.Abs(h) > 0)
         {
             cueTransform.RotateAround(pos, Vector3.up, rotationSpeed * h * Time.deltaTime);
-           // Debug.Log(cueTransform.rotation.eulerAngles.x);
+            // Debug.Log(cueTransform.rotation.eulerAngles.x);
         }
         Vector3 flickD = -cueTransform.forward;
         if (Input.GetButton("Jump"))
@@ -39,5 +66,32 @@ public class GameControl : MonoBehaviour
             Vector3 flick = new Vector3(flickD.x, 0, flickD.z);
             cueBallRigidBody.AddForce(flickSize * flick, ForceMode.Impulse);
         }
+    }
+
+    int CountScore()
+    {
+        int count = 0;
+        foreach (GameObject g in targets)
+        {
+            if (!g.activeSelf)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!done)
+        {
+            UpdateCue();
+            int count = CountScore();            
+            if (count == numberOfTargets)
+            {
+                done = true;
+            }
+        }
+        
     }
 }
